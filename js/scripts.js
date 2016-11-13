196156def22cfe7b4df56d410a4f51a6610470f4
Samantha Hill <samhill5643@gmail.com>
2016-11-13 10:46:37 -0500

    Clean up Formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
 //AJAX Call - OMDB
 var omdbURL = "https://www.omdbapi.com/?s=harry+potter&r=json";
 $.getJSON(omdbURL, displayMovies);
+
 //Get info for film
 function displayMovies(data) {
   var movieHTML = '<ul id="filmGallery">';
-  $.each(data.Search, function (i, movie){
+  $.each(data.Search, function (i, movie) {
     movieHTML += '<li>';
     movieHTML += '<img src="' + movie.Poster + '">';
     movieHTML += '<h3>' + movie.Title + '</h3>';
     movieHTML += '<p class="hidden">' + movie.imdbID + '</p>';
     movieHTML += '<p class="hidden">' + movie.Year + '</p></li>';
   });
-    movieHTML += '</ul>';
-    $('#film').html(movieHTML);
-    $("li:nth-child(2)").hide();//img link is broken, this hides it
+  movieHTML += '</ul>';
+  $('#film').html(movieHTML);
+  $("li:nth-child(2)").hide();//img link is broken, this hides it
 }
+
 //Put film photo on overlay
 function showFilm(item) {
   var displayPoster = item.children("img").attr("src");
   $('#overlay img').attr("src", displayPoster);
 
-//Use movie ID to look up plot
+  //Use movie ID to look up plot
   var displayID = item.children("p").first().text();
   var displayTitle = item.children("h3").text();
   var displayYear = item.children("p").last().text();
   var omdbURLById = "https://www.omdbapi.com/?i=" + displayID + "&r=json";
-  var movie;
   $.ajax(omdbURLById, {
     complete: function(newXHR){
-      movie = $.parseJSON(newXHR.responseText);
-      $('#overlay span').html("<p>" + "Title: " + displayTitle + "</p>" + "<p>" + "Year: " + displayYear + "</p>" + "<p>" + "Plot: " + movie.Plot + "</p>");
+      var film = $.parseJSON(newXHR.responseText);
+      $('#overlay span').html("<p>" + "Title: " + displayTitle + "</p>" + "<p>" + "Year: " + displayYear + "</p>" + "<p>" + "Plot: " + film.Plot + "</p>");
     }
   });
 
@@ -42,6 +43,7 @@ $("#film").on("click", "li", function (item) {
   var item_to_show = $(this);
   showFilm(item_to_show);
 });
+
 //Sort by year button
 $('#yearSort').click(function() {
     var filmPhotos = $('#filmGallery li');
